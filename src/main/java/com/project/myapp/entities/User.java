390868d59f0f46b270e14c72a5c0a257ea63ea60
adem kok <ademk760@gmail.com>
2023-09-04 13:31:37 +0200

    myapp erste Push

---
+++ src/main/java/com/project/myapp/entities/User.java
@@ -0,0 +1,20 @@
+package com.project.myapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "user")
+@Data
+public class User {
+
+	@Id
+	Long id;
+	
+	String userName;
+	String password;
+	
+}
