1cbaf87df3dfab1f801fd7ff5d2188cadb3a1e12
adem kok <ademk7604@gmail.com>
2023-12-05 13:05:42 +0100

    custom Exception

--- src/main/java/com/project/myapp/controllers/UserController.java
+++ src/main/java/com/project/myapp/controllers/UserController.java
@@ -2,16 +2,20 @@ package com.project.myapp.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.myapp.entities.User;
+import com.project.myapp.exceptions.UserNotFoundException;
 import com.project.myapp.responses.UserResponse;
 import com.project.myapp.services.UserService;
 
@@ -37,9 +41,12 @@ public class UserController {
 	}
 
 	@GetMapping("/{userId}")
-	public User getOneUser(@PathVariable Long userId) {
-		// custom exception
-		return userService.getOneUserById(userId);
+	public UserResponse getOneUser(@PathVariable Long userId) {
+		User user = userService.getOneUserById(userId);
+		if(user == null) {
+			throw new UserNotFoundException();
+		}
+		return new UserResponse(user);
 	}
 
 	@PutMapping("/{userId}")
@@ -58,6 +65,12 @@ public class UserController {
 	@GetMapping("/activity/{userId}")
 	public List<Object> getUserActivity(@PathVariable Long userId) {
 		return userService.getUserActivity(userId);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	private void handleUserNotFound() {
+		
 	}
 
 
